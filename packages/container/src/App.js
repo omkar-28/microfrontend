d03da15cc04b68bb90d0cf96e0987105ab055158
omkar <omikhatavkar2@gmail.com>
2024-09-09 21:08:48 +0530

    Finished feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,5 +2,9 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    return <MarketingApp />
+    return (
+        <div>
+            <h1>Container App</h1>
+            <MarketingApp />
+        </div>)
 }
