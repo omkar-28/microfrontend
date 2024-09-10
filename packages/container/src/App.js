d2df8e58a97d6fdf9448f66786935a4cf247fb81
omkar <omikhatavkar2@gmail.com>
2024-09-10 16:35:27 +0530

    Added dasboard vue and yml file.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense, useState, useEffect } from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
@@ -11,6 +11,7 @@ import Header from './components/Header';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
@@ -19,7 +20,14 @@ const generateClassName = createGenerateClassName({
 const history = createBrowserHistory();
 
 export default () => {
-  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignedIn])
+
   return (
     <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
@@ -32,6 +40,10 @@ export default () => {
             <Switch>
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
               </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -22,6 +23,9 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap',
             },
             shared: packageJson.dependencies,
+        }),
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
         }),
     ],
 };
